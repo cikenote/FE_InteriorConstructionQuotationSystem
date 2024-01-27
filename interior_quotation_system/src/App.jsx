2ec767b827efab9cca92f64f6c613e0a33c4a190
hnamredips <nghnam265@gmail.com>
2024-01-27 10:15:26 +0700

    Create component Error

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
+import Page_404 from "./components/Error/Error";
 import HomePage from "./pages/home/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import ProjectPage from "./pages/project/ProjectPage";
 // import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
 function App() {
-  return (
-
-    <HomePage />
-    // <ProjectPage/>
-    // <ProjectSingle/>
-  );
+    return (
+        // <HomePage />
+        // <ProjectPage/>
+        // <ProjectSingle/>
+        <Page_404 />
+    );
 }
 
 export default App;
---
+++ interior_quotation_system/src/components/Error/Error.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../../styles/components/error.scss";
+
+const Error = () => {
+    const ClickHandler = () => {
+        window.scrollTo(10, 0);
+    };
+
+    return (
+        <section className="error-404-section section-padding">
+            <div className="container">
+                <div className="row">
+                    <div className="col col-xs-12">
+                        <div className="content clearfix">
+                            <div className="error">
+                                <img src="./images/error-404.svg" alt="ERROR" />
+                            </div>
+                            <div className="error-message">
+                                <h3>Oops! Page Not Found!</h3>
+                                <p>
+                                    We’re sorry but we can’t seem to find the
+                                    page you requested. This might be because
+                                    you have typed the web address incorrectly.
+                                </p>
+                                <a
+                                    onClick={ClickHandler}
+                                    href="/home"
+                                    className="theme-btn"
+                                >
+                                    Back to home
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Error;

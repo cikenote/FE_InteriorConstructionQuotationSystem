33d8f53778f3104310d2e9ad8e993efc4fb72bf0
quangthanh66 <126049385+quangthanh66@users.noreply.github.com>
2024-01-28 11:52:26 +0700

    Revert "update module contact"

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -1,18 +1,15 @@
 import "./App.css";
 import Page_404 from "./components/Error/Error";
-import ContactPage from "./pages/contact/ContactPage";
 import HomePage from "./pages/home/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
-
- import ProjectPage from "./pages/project/ProjectPage";
+// import ProjectPage from "./pages/project/ProjectPage";
 // import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
 function App() {
     return (
-       //  <HomePage />
-         <ProjectPage/>
+        // <HomePage />
+        // <ProjectPage/>
         // <ProjectSingle/>
-       // <Page_404 />
-       // <ContactPage />
+        <Page_404 />
     );
 }
 

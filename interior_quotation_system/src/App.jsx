3b9ac0cd932587af8f5ca9136ac74c02b6ce18aa
Cong Danh <chucongdanh2703@gmail.com>
2024-01-28 13:14:56 +0700

    resole conflic

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-<<<<<<< danh-dev
 import ContactPage from "./pages/contact/ContactPage";
 import HomePage from "./pages/home/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -20,19 +19,5 @@ function App() {
     </Routes>
   );
 
-import Page_404 from "./components/Error/Error";
-import HomePage from "./pages/home/HomePage";
-import "bootstrap/dist/css/bootstrap.min.css";
-// import ProjectPage from "./pages/project/ProjectPage";
-// import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
-function App() {
-    return (
-        // <HomePage />
-        // <ProjectPage/>
-        // <ProjectSingle/>
-        <Page_404 />
-    );
->>>>>>> main
 }
-
 export default App;

0ec3aa416d8bcbd438aa457d9ab6585ed0f32f1a
Cong Danh <chucongdanh2703@gmail.com>
2024-01-28 11:59:33 +0700

    setup react-router-dome

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -1,19 +1,20 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Page_404 from "./components/Error/Error";
 import ContactPage from "./pages/contact/ContactPage";
 import HomePage from "./pages/home/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
 
- import ProjectPage from "./pages/project/ProjectPage";
-// import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
+import ProjectPage from "./pages/project/ProjectPage";
+import BlogPage from "./pages/blog/BlogPage";
 function App() {
-    return (
-       //  <HomePage />
-         <ProjectPage/>
-        // <ProjectSingle/>
-       // <Page_404 />
-       // <ContactPage />
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/project" element={<ProjectPage />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+    </Routes>
+  );
 }
 
 export default App;

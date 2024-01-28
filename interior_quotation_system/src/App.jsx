c9f7edd6f52e054e089a86438adb3491f0e2a094
quangthanh66 <quangthanhbt66@gmail.com>
2024-01-28 13:07:15 +0700

    update module contact

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import Page_404 from "./components/Error/Error";
 import HomePage from "./pages/home/HomePage";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ContactPage from "./pages/contact/ContactPage";
 // import ProjectPage from "./pages/project/ProjectPage";
 // import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
 function App() {
     return (
-        // <HomePage />
+      //   <HomePage />
         // <ProjectPage/>
         // <ProjectSingle/>
-        <Page_404 />
+        //<Page_404 />
+        <ContactPage />
     );
 }
 

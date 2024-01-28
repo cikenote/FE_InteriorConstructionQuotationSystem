3a3677b910e8411757822c8043d3a7f21ebbd7d5
Cong Danh <chucongdanh2703@gmail.com>
2024-01-28 12:57:31 +0700

    update react-router-dom

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ContactPage from "./pages/contact/ContactPage";
 import HomePage from "./pages/home/HomePage";
@@ -6,11 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import ProjectPage from "./pages/project/ProjectPage";
 import BlogPage from "./pages/blog/BlogPage";
+import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/project" element={<ProjectPage />} />
+      <Route path="/project/project-single" element={<ProjectSingle />} />
       <Route path="/blog" element={<BlogPage />} />
       <Route path="/contact" element={<ContactPage />} />
     </Routes>

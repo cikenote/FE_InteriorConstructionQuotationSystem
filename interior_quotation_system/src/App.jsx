e20e91f3e55a34f7a3aa467846e9aaceb39513e1
quangthanh <quangthanh66>
2024-01-30 00:05:51 +0700

    uploadig

--- interior_quotation_system/src/App.jsx
+++ interior_quotation_system/src/App.jsx
@@ -6,16 +6,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import ProjectPage from "./pages/project/ProjectPage";
 import ProjectSingle from "./components/ProjectSingle/ProjectSingle";
 import BlogPage from "./pages/blog/BlogPage";
+import ShopItem from "./pages/shop";
+import ItemDetail from "./pages/shop/ItemDetail";
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/project" element={<ProjectPage />} />
-            <Route path="/project/project-single" element={<ProjectSingle />} />
-            <Route path="/blog" element={<BlogPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/project" element={<ProjectPage />} />
+      <Route path="/project/project-single" element={<ProjectSingle />} />
+      <Route path="/blog" element={<BlogPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/shop" element={<ShopItem />} />
+      <Route path="/shop/item/:id" element={<ItemDetail />} />
+    </Routes>
+  );
 }
 export default App;
--- interior_quotation_system/src/pages/shop/index.jsx
+++ interior_quotation_system/src/pages/shop/index.jsx
@@ -1,7 +1,134 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import "../../styles/components/shop.scss";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const ShopItem = () => {
-  return <div>ShopItem</div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="landing-page">
+        <div className="content">
+          <h3 className="title">Shop</h3>
+          <div className="links">
+            <div className="home-link">Home</div>
+            <div className="dot"></div>
+            <div>Shop</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="products">
+        <div className="product-list">
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+
+          <div className="product-detail">
+            <img
+              alt="product-alt"
+              src="https://arkio-next.netlify.app/images/shop/1.jpg"
+              className="image"
+            />
+            <div className="content">
+              <a className="title" href="/shop/item/123">
+                Black Timber Chairs
+              </a>
+              <p className="price">$65.00</p>
+              <div className="button-cart">Add to cart</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="pagination">
+        <div className="content">
+          <div className="item">
+            <IoIosArrowBack />
+          </div>
+          <div className="item active">1</div>
+          <div className="item">2</div>
+          <div className="item">3</div>
+          <div className="item">
+            <IoIosArrowForward />
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default ShopItem;

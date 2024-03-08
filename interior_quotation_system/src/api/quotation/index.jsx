393679ed2ef64aef21438221b1f23663a083cbfc
quangthanh66 <quangthanh66@gmail.com>
2024-03-08 17:38:46 +0700

    uploading product

--- interior_quotation_system/src/api/quotation/index.jsx
+++ interior_quotation_system/src/api/quotation/index.jsx
@@ -4,6 +4,7 @@ import { END_POINT_API } from "../endpoint";
 const QuotationAPI = {
   AddNewQuotation: (params) =>
     axiosClient.post(END_POINT_API.QUOTATION, params),
+  GetQuotationsList: () => axiosClient.get(END_POINT_API.QUOTATION),
 };
 
 export default QuotationAPI;
--- interior_quotation_system/src/components/QuotationForm/QuotationForm.jsx
+++ interior_quotation_system/src/components/QuotationForm/QuotationForm.jsx
@@ -1,68 +1,66 @@
-import "../../styles/components/quotationForm.scss"
+import { useEffect, useState } from "react";
+import "../../styles/components/quotationForm.scss";
 import TableBody from "../TableBody/TableBody";
 import TableHeader from "../TableHeader/TableHeader";
 const QuotationForm = () => {
-    return (
-        <div className="quotation-form" id='dangpro'>
-            <div className="logo">
-            </div>
-            <div className="text-container">
-                <div className="left-text">
-                    <div>
-                        <strong>Email:</strong>
-                        <span>daihocfpt@fpt.edu.vn</span>
-                    </div>
-                    <div>
-                        <strong>Số điện thoại:</strong>
-                        <span>028 7300 1866</span>
-                    </div>
-                    <div>
-                        <strong>Showroom:</strong>
-                        <span>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, TP. Hồ Chí Minh</span>
-                    </div>
-                    <div>
-                        <strong>Xưởng sản xuất:</strong>
-                        <span>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, TP. Hồ Chí Minh</span>
-                    </div>
-                </div>
-                <div className="right-text">
-                    <div>
-                        <strong>Khách hàng:</strong>
-                        <input
-                            type="text"
-                            placeholder="Nhập họ và tên"
-                        />
-                    </div>
-                    <div>
-                        <strong>Số điện thoại:</strong>
-                        <input
-                            type="text"
-                            placeholder="Nhập số điện thoại"
-                        />
-                    </div>
-                    <div>
-                        <strong>Địa chỉ công trình:</strong>
-                        <input
-                            type="text"
-                            placeholder="Nhập địa chỉ"
-                        />
-                    </div>
-                    <div>
-                        <strong>Ngày soạn báo giá:</strong>
-                        <input
-                            type="text"
-                        />
-                    </div>
-                </div>
-            </div>
-            <TableHeader />
-            <TableBody/>
-            <div className="note-2">
-                <p className="note-text">
-                    Báo giá trên chỉ mang tính chất tham khảo do chưa có số liệu công trình thực tế. Vui lòng liên hệ sale:
-                </p>
-            </div>
-            {/* <Joyride
+  const [quotation, setQuotation] = useState([]);
+
+  return (
+    <div className="quotation-form" id="dangpro">
+      <div className="logo"></div>
+      <div className="text-container">
+        <div className="left-text">
+          <div>
+            <strong>Email:</strong>
+            <span>daihocfpt@fpt.edu.vn</span>
+          </div>
+          <div>
+            <strong>Số điện thoại:</strong>
+            <span>028 7300 1866</span>
+          </div>
+          <div>
+            <strong>Showroom:</strong>
+            <span>
+              Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ
+              Đức, TP. Hồ Chí Minh
+            </span>
+          </div>
+          <div>
+            <strong>Xưởng sản xuất:</strong>
+            <span>
+              Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ
+              Đức, TP. Hồ Chí Minh
+            </span>
+          </div>
+        </div>
+        <div className="right-text">
+          <div>
+            <strong>Khách hàng:</strong>
+            <input type="text" placeholder="Nhập họ và tên" />
+          </div>
+          <div>
+            <strong>Số điện thoại:</strong>
+            <input type="text" placeholder="Nhập số điện thoại" />
+          </div>
+          <div>
+            <strong>Địa chỉ công trình:</strong>
+            <input type="text" placeholder="Nhập địa chỉ" />
+          </div>
+          <div>
+            <strong>Ngày soạn báo giá:</strong>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <TableHeader />
+      <TableBody />
+      <div className="note-2">
+        <p className="note-text">
+          Báo giá trên chỉ mang tính chất tham khảo do chưa có số liệu công
+          trình thực tế. Vui lòng liên hệ sale:
+        </p>
+      </div>
+      {/* <Joyride
             steps={steps}
             run={isTourOpen}
             continuous={true}
@@ -88,9 +86,8 @@ const QuotationForm = () => {
               },
             }}
           /> */}
-        </div>
+    </div>
+  );
+};
 
-    );
-}
-
-export default QuotationForm
+export default QuotationForm;

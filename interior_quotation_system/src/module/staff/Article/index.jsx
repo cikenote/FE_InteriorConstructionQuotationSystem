ce3d04903be40501306b24af24981d64f9d10e75
hnad273 <chucongdanh2703@gmail.com>
2024-03-28 20:07:34 +0700

    update dashboard

--- interior_quotation_system/src/module/staff/Article/index.jsx
+++ interior_quotation_system/src/module/staff/Article/index.jsx
@@ -2,23 +2,47 @@ import React, { useRef } from "react";
 import TableLayout from "../../../layouts/TableLayout";
 import { ARTICLE_COLUMNS, ARTICLE_DATA_SOURCE } from "./constant";
 import ArticleModal from "./ArticleModal";
+import Table from 'react-bootstrap/Table';
 
 const StaffArticle = () => {
   const articleActionModal = useRef();
 
-  const searchStaffArticle = (event) => {};
+  const searchStaffArticle = (event) => { };
 
   return (
     <>
       <ArticleModal ref={articleActionModal} />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>@fat</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
 
-      <TableLayout
-        tableColumns={ARTICLE_COLUMNS}
-        tableDataSource={ARTICLE_DATA_SOURCE}
-        actionName={"New article"}
-        onchangeSearch={searchStaffArticle}
-        addNewAction={() => articleActionModal.current.openModal()}
-      />
     </>
   );
 };
--- interior_quotation_system/src/module/staff/Dashboard/index.jsx
+++ interior_quotation_system/src/module/staff/Dashboard/index.jsx
@@ -1,24 +1,93 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TableLayout from "../../../layouts/TableLayout";
 import { PROJECT_COLUMNS, PROJECT_DATA_SOURCE } from "./constant";
 import ProjectModal from "./ProjectModal";
-
+import "./style.scss"
+import RegisterPage from './../../../pages/register/RegisterPage';
+import axios from "axios";
 const StaffProjects = () => {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [contracts, setContracts] = useState([]);
+
   const projectActionModal = useRef();
 
-  const searchStaffProject = (event) => {};
+  const searchStaffProject = (event) => { };
 
+  const fetchGetNumberProducts = async () => {
+    const response = await axios.get("https://swp391api.developvn.click/api/Dashboard/GetNumberProducts");
+    setProducts(response.data)
+  }
+
+  const fetchGetNumberCategory = async () => {
+    const response = await axios.get("https://swp391api.developvn.click/api/Dashboard/GetNumberCategory");
+    setCategory(response.data)
+  }
+
+  const fetchGetNumberUsers = async () => {
+    const response = await axios.get("https://swp391api.developvn.click/api/Dashboard/GetNumberUser");
+    setUsers(response.data)
+  }
+  
+  const fetchGetNumberContract = async () => {
+    const response = await axios.get("https://swp391api.developvn.click/api/Dashboard/GetNumberContract");
+    setContracts(response.data)
+  }
+
+  useEffect(() => {
+    fetchGetNumberProducts();
+    fetchGetNumberCategory();
+    fetchGetNumberUsers();
+    fetchGetNumberContract();
+  }, [])
   return (
     <>
       <ProjectModal ref={projectActionModal} />
-
-      <TableLayout
-        tableColumns={PROJECT_COLUMNS}
-        tableDataSource={PROJECT_DATA_SOURCE}
-        actionName={"New Project"}
-        onchangeSearch={searchStaffProject}
-        addNewAction={() => projectActionModal.current.openModal()}
-      />
+      <div className="content-dashboard">
+        <div className="row">
+          <div className="col-xl-3 col-sm-6 mb-3">
+            <div className="card text-white bg-primary o-hidden h-100">
+              <div className="card-body">
+                <div className="card-body-icon">
+                  <i className="fa fa-fw fa-comments"></i>
+                </div>
+                <div className="mr-5">{products} Products</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 mb-3">
+            <div className="card text-white bg-warning o-hidden h-100">
+              <div className="card-body">
+                <div className="card-body-icon">
+                  <i className="fa fa-fw fa-list"></i>
+                </div>
+                <div className="mr-5">{category} Category</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 mb-3">
+            <div className="card text-white bg-success o-hidden h-100">
+              <div className="card-body">
+                <div className="card-body-icon">
+                  <i className="fa fa-fw fa-shopping-cart"></i>
+                </div>
+                <div className="mr-5">{users} Users</div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-3 col-sm-6 mb-3">
+            <div className="card text-white bg-danger o-hidden h-100">
+              <div className="card-body">
+                <div className="card-body-icon">
+                  <i className="fa fa-fw fa-support"></i>
+                </div>
+                <div className="mr-5">{contracts} New Tickets!</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };

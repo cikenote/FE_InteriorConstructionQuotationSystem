05a31f61ecc133112ddb74c1d9f9fc2020a055cc
hnad273 <chucongdanh2703@gmail.com>
2024-03-29 00:33:51 +0700

    update staff articles

--- interior_quotation_system/src/module/staff/Article/ArticleModal.jsx
+++ interior_quotation_system/src/module/staff/Article/ArticleModal.jsx
@@ -1,28 +1,53 @@
-import { Modal, Form, Row, Col, Input, DatePicker } from "antd";
-import { forwardRef, useImperativeHandle, useState } from "react";
+import { Modal, Form, Row, Col, Input, Button } from "antd";
+import { forwardRef, useImperativeHandle, useState, useEffect } from "react";
+import axios from "axios";
 import { FORM_RULES } from "../../../utils/constant";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const ArticleModal = ({ articleUpdate }, ref) => {
+const ArticleModal = forwardRef(({ selectedBlog }, ref) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [form] = Form.useForm();
 
-  useImperativeHandle(ref, () => {
-    return {
-      openModal: () => setIsOpenModal(true),
-    };
-  });
+  useImperativeHandle(ref, () => ({
+    openModal: () => {
+      setIsOpenModal(true);
+    },
+  }));
+
+  useEffect(() => {
+    if (isOpenModal && selectedBlog) {
+      form.setFieldsValue({
+        title: selectedBlog.title,
+        content: selectedBlog.content,
+        img: selectedBlog.image
+      });
+    }
+  }, [isOpenModal, selectedBlog, form]);
 
   const onCloseModal = () => {
     setIsOpenModal(false);
   };
 
-  const onFinishForm = (values) => {};
+  const onFinishForm = async (values) => {
+    try {
+      // Gửi dữ liệu lên server bằng phương thức PUT
+      await axios.put(`https://swp391api.developvn.click/api/Articles/${selectedBlog.id}`, values);
+      console.log("Updated blog:", values);
+      toast.success('Cập nhật thành công!');
+      onCloseModal();
+    } catch (error) {
+      console.error("Error updating blog:", error);
+      toast.error('Cập nhật thất bại. Vui lòng thử lại sau!');
+    }
+  };
 
   return (
     <Modal
-      title={articleUpdate ? "Update article" : "New Article"}
-      open={isOpenModal}
+      title="Edit Article"
+      visible={isOpenModal}
       onCancel={onCloseModal}
+      footer={null}
     >
       <Form form={form} onFinish={onFinishForm} layout="vertical">
         <Row gutter={[10, 10]}>
@@ -33,46 +58,27 @@ const ArticleModal = ({ articleUpdate }, ref) => {
           </Col>
 
           <Col span={24}>
-            <Form.Item name="style" label="Style" rules={[FORM_RULES.required]}>
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              name="startDate"
-              label="Start date"
-              rules={[FORM_RULES.required]}
-            >
-              <DatePicker style={{ width: "100%" }} />
-            </Form.Item>
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              name="endDate"
-              label="End date"
-              rules={[FORM_RULES.required]}
-            >
-              <DatePicker style={{ width: "100%" }} />
-            </Form.Item>
-          </Col>
-
-          <Col span={24}>
-            <Form.Item name="description" label="Description" required={false}>
+            <Form.Item name="content" label="Content" rules={[FORM_RULES.required]}>
               <Input.TextArea rows={3} />
             </Form.Item>
           </Col>
 
           <Col span={24}>
-            <Form.Item name="image" label="Image" required={false}>
+            <Form.Item name="img" label="Image" required={false}>
               <Input />
             </Form.Item>
+          </Col>
+        </Row>
+        <Row justify="end">
+          <Col>
+            <Button type="primary" htmlType="submit">
+              Update
+            </Button>
           </Col>
         </Row>
       </Form>
     </Modal>
   );
-};
+});
 
-export default forwardRef(ArticleModal);
+export default ArticleModal;

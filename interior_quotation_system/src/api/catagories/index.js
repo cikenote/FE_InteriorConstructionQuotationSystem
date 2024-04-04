674f341d12a4a4a9b529ca6c7394f736ea9c0cea
quangthanh66 <quangthanh66@gmail.com>
2024-04-04 19:09:02 +0700

    Revert "resolsved conflict"
    This reverts commit 38d347fd18730e6ec686b170b33fb14946d6e2d2.

--- interior_quotation_system/src/api/catagories/index.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "../axiosClient";
-import { END_POINT_API } from "../endpoint";
-
-const CategoryAPI = {
-  getcategorylist: () => axiosClient.get(END_POINT_API.CATEGORY),
-};
-
-export default CategoryAPI;
